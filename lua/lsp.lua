abe3f176a2b8b35b8f6fecddb72af22a2cb94f48
Forthoney <castlehoneyjung@gmail.com>
2023-09-11 17:49:23 -0400

    implement cmp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,25 +1,46 @@
 require("neodev").setup({})
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
 local lspconfig = require("lspconfig")
 
-lspconfig.rubocop.setup({})
+local on_attach_universal = function(_, bufnr)
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = "LSP: " .. desc
+        end
 
-vim.opt.signcolumn = "yes"
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "ruby",
-    callback = function()
-        vim.lsp.start({
-            name = "rubocop",
-            cmd = { "bundle", "exec", "rubocop", "--lsp" },
-        })
-    end,
-})
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "ruby",
-    callback = function()
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+    nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+    -- See `:help K` for why this keymap
+    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+    nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+    -- Lesser used LSP functionality
+    nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+    nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+    nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+    nmap("<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, "[W]orkspace [L]ist Folders")
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
         vim.lsp.buf.format()
-    end,
-})
+    end, { desc = "Format current buffer with LSP" })
+end
 
 -- textDocument/diagnostic support until 0.10.0 is released
 _timers = {}
@@ -66,19 +87,20 @@ end
 lspconfig.ruby_ls.setup({
     on_attach = function(client, buffer)
         setup_diagnostics(client, buffer)
+        on_attach_universal(client, buffer)
     end,
+    capabilities = capabilities,
 })
 
 -- example to setup lua_ls and enable call snippets
 lspconfig.lua_ls.setup({
+    on_attach = on_attach_universal,
+    capabilities = capabilities,
     settings = {
         Lua = {
-            completion = {
-                callSnippet = "Replace",
-            },
-            workspace = {
-                checkThirdParty = false,
-            },
+            completion = { callSnippet = "Replace" },
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
         },
     },
 })
