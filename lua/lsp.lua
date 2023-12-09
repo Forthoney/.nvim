48a4a9fdac2b1abd084f359d0cc487f2a3ab4ddf
Forthoney <castlehoneyjung@gmail.com>
2023-12-09 16:32:03 -0500

    fix comment

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,57 +1,42 @@
 require("neodev").setup({})
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
 local lspconfig = require("lspconfig")
 
--- vim.api.nvim_create_autocmd('LspAttach', {
---     group = vim.api.nvim_create_augroup('UserLspConfig', {})
---     callback = function (ev)
---         vim.bo[ev.buf].omnifunc = 'v'
---     end
--- })
-
-local on_attach = function(_, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        local nmap = function(keys, func, desc)
+            if desc then
+                desc = "LSP: " .. desc
+            end
+            vim.keymap.set("n", keys, func, { buffer = ev.buf, desc = desc })
         end
 
-        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-    end
+        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+        nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+        nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+        -- See `:help K` for why this keymap
+        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+        nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+        nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+        nmap("<leader>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, "[W]orkspace [L]ist Folders")
 
-    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-    nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-    nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-    nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-    -- See `:help K` for why this keymap
-    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-    nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-    -- Lesser used LSP functionality
-    nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-    nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-    nmap("<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, "[W]orkspace [L]ist Folders")
-
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-        vim.lsp.buf.format()
-    end, { desc = "Format current buffer with LSP" })
-end
+        nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+        nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    end,
+})
 
 -- textDocument/diagnostic support until 0.10.0 is released
-_timers = {}
 local function setup_diagnostics(client, buffer)
+    local _timers = {}
     if require("vim.lsp.diagnostic")._enable then
         return
     end
@@ -97,16 +82,15 @@ lspconfig.ruby_ls.setup({
         setup_diagnostics(client, buffer)
     end,
 })
-
-lspconfig.steep.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
+-- lspconfig.steep.setup({})
+lspconfig.lua_ls.setup({})
+lspconfig.ocamllsp.setup({})
+lspconfig.pyright.setup({})
+lspconfig.tsserver.setup({})
+lspconfig.rust_analyzer.setup({})
 
 -- example to setup lua_ls and enable call snippets
 lspconfig.lua_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
     settings = {
         Lua = {
             completion = { callSnippet = "Replace" },
@@ -114,24 +98,4 @@ lspconfig.lua_ls.setup({
             telemetry = { enable = false },
         },
     },
-})
-
-lspconfig.ocamllsp.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
-
-lspconfig.pyright.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
-
-lspconfig.tsserver.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-})
-
-lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,6 +10,11 @@ return {
     "rose-pine/neovim",
     "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
+    { "numToStr/Comment.nvim", opts = {} }, -- "gc" to comment visual regions/lines
+    { "windwp/nvim-ts-autotag", opts = {} },
+    { "folke/neodev.nvim", opts = {} },
+    { "williamboman/mason.nvim", opts = {} },
+
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
     {
         "nvim-telescope/telescope-fzf-native.nvim",
